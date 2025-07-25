5ec63179d0ff97388094193ff6574b7912353f40
Docile123 <imbereyemasoo@gmail.com>
2025-07-26 01:11:55 +0200

    feat:Adding another part of top bar

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import image from '../assets/image.png'
+import image from '../assets/image.png';
+import imageleft from '../assets/image-left.png';
+import icons from '../assets/icons.png';
+
 
 const Top = () => {
   return (
-     <main className='mt-20 '>
+     <main className='mt-20 mr-20 max-lg:mr-0'>
        <div className='flex lg:flex-row max-lg:flex-col max-lg:gap-y-5 '>
                 <div className='space-y-5 lg:space-y-10'>
         <h1 className='text-5xl font-medium text-gray-800 max-lg:text-3xl'>Want anything to be easy with <span className='text-gray-900 font-extrabold'>LasleyVPN</span></h1>
@@ -16,7 +19,7 @@ const Top = () => {
 
        </div>
 
-       <div className='grid lg:grid-cols-3  shadow-xl mx-20 mt-10 py-5 max-lg:mx-0'>
+       <div className='grid lg:grid-cols-3  shadow-xl  mt-10 py-5 '>
               <div className='small-card'>
                <i class="fa-solid fa-user small-card-icon"></i>
                <div>
@@ -42,6 +45,41 @@ const Top = () => {
                </div>   
           </div>
        </div>
+      <div className='flex lg:flex-row max-lg:flex-col max-lg:gap-y-5 mt-10 lg:mt-20 shadow-lg py-4 bg-white max-lg:px-2'>
+          <div className='lg:p-10'>
+              <img src={imageleft} alt="" className='w-full object-cover lg:w-150' />  
+             </div>
+                <div className='space-y-5 lg:space-y-10'>
+        <h1 className='text-5xl font-medium text-gray-800 max-lg:text-3xl'>We provide Many Features You Can Use</h1>
+        <p className='text-lg/7 tracking-wide text-gray-500'>You can explore the features that we provide with fun and have their own functions each features</p>
+          <ul className='space-y-5 mt-3'>
+             <li className='flex items-center text-gray-500 font-medium gap-x-3 group transition ease-in-out'>
+               <img src={icons} alt=""  className='size-10 group-hover:grayscale'/>
+               <p className='group-hover:text-gray-900/50'>Powerfull online Protection.</p>
+             </li>
+             <li className='flex items-center text-gray-500 font-medium gap-x-3 group transition ease-in-out'>
+               <img src={icons} alt="" className='size-10 group-hover:grayscale'/>
+               <p className='group-hover:text-gray-900/50'>Internet without borders.</p>
+             </li>
+             <li className='flex items-center text-gray-500 font-medium gap-x-3 group transition ease-in-out'>
+               <img src={icons} alt="" className='size-10 group-hover:grayscale'/>
+               <p className='group-hover:text-gray-900/50'>Supercharged VPN.</p>
+             </li>
+             <li className='flex items-center text-gray-500 font-medium gap-x-3 group transition ease-in-out'>
+               <img src={icons} alt="" className='size-10 group-hover:grayscale'/>
+               <p className='group-hover:text-gray-900/50'>No specific time limits.</p>
+             </li>
+          </ul>
+      </div>
+             
+
+       </div>
+   
+   <div className='lg:mt-15 max-lg:mt-10 flex flex-col items-center max-lg:text-xl'> 
+      <h1 className='text-3xl font-extrabold text-gray-900 tracking-wide mb-5'>Choose Your Plan</h1>
+      <p className='text-gray-500 text-lg leading-relaxed tracking-widest max-lg:text-sm'>Let's choose the package that is best for you and explore it happly an cheerfully</p>
+   </div>
+
      
      </main>
   )
