9564009c7d2407817704b2644467b94cdc01097a
Docile123 <imbereyemasoo@gmail.com>
2025-07-26 15:58:42 +0200

    feat:finalizing the design of footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,25 +3,25 @@ import logo from '../assets/logo.png';
 
 const Footer = () => {
   return (
-    <div className='bg-gray-100   lg:px-20  lg:py-30 px-10 flex flex-col lg:flex-row lg:gap-x-30'>
-         <div className='lg:w-80'>
-            <img src={logo} alt="" className='w-32 lg:mb-4' />
-             <p className='text-gray-700 text-base/5'>LaslesVPN is a private virtual network that has unique features and has high security</p>
+    <div className='bg-gray-100   lg:px-20  lg:py-30 px-10 flex flex-col lg:flex-row lg:gap-x-30 max-lg:py-40'>
+         <div className='w-100 max-lg:w-fit'>
+            <img src={logo} alt="" className='w-32 mb-4' />
+             <p className='text-gray-700 text-base/5 '>LaslesVPN is a private virtual network that has unique features and has high security</p>
              <div className='space-x-5 mt-5'>
               <i class="fa-brands fa-facebook bg-white p-2 rounded-full ring-1 ring-red-300 text-red-500 text-sm shadow-2xl"></i>
               <i class="fa-brands fa-twitter bg-white p-2 rounded-full ring-1 ring-red-300 text-red-500 text-sm shadow-2xl"></i>
               <i class="fa-brands fa-square-instagram bg-white p-2 rounded-full ring-1 ring-red-300 text-red-500 text-sm shadow-2xl"></i>
               
              </div>
-             <p className='mt-7 text-gray-500 font-medium text-base'>&copy;{new Date().getFullYear()}LaslesVPN</p>
+             <p className='mt-7 text-gray-500 font-medium text-base max-lg:mb-4'>&copy;{new Date().getFullYear()}LaslesVPN</p>
          </div>
 
 
 <div className='lg:flex lg:flex-row justify-between w-full'>
   
-         <div>
-           <h1>Product</h1>
-           <ul>
+         <div className='max-lg:border-t-1 border-t-neutral-300 max-lg:pt-3'>
+           <h1 className='font-semibold mb-4 max-lg:mb-2 max-lg:text-lg'>Product</h1>
+           <ul className='text-base text-gray-500 font-mono  space-y-3 '>
             <li>Download</li>
             <li>Pricing</li>
             <li>Locations</li>
@@ -30,26 +30,24 @@ const Footer = () => {
             <li>Blog</li>
            </ul>
          </div>
-           <div>
-           <h1>Product</h1>
-           <ul>
-            <li>Download</li>
-            <li>Pricing</li>
-            <li>Locations</li>
-            <li>Server</li>
-            <li>Countries</li>
-            <li>Blog</li>
+             
+          <div className='max-lg:border-t-1 border-t-neutral-300 max-lg:pt-3 max-lg:mt-5'>
+           <h1 className='font-semibold mb-4 max-lg:mb-2 max-lg:text-lg'>Engage</h1>
+           <ul className='text-base text-gray-500 font-mono  space-y-3 '>
+            <li>LaslesVPN ?</li>
+            <li>FAQ</li>
+            <li>Tutorials</li>
+            <li>About us</li>
+            <li>Privacy Policy</li>
+            <li>Terms of service</li>
            </ul>
          </div>
-           <div>
-           <h1>Product</h1>
-           <ul>
-            <li>Download</li>
-            <li>Pricing</li>
-            <li>Locations</li>
-            <li>Server</li>
-            <li>Countries</li>
-            <li>Blog</li>
+
+         <div className='max-lg:border-t-1 border-t-neutral-300 max-lg:pt-3 max-lg:mt-5'>
+           <h1 className='font-semibold mb-4 max-lg:mb-2 max-lg:text-lg'>Earn Money</h1>
+           <ul className='text-base text-gray-500 font-mono  space-y-3 '>
+            <li>Affiliate</li>
+            <li>Become Patner</li>
            </ul>
          </div>
 </div>
